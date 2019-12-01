0dd960b14135d4831dae11b30647a068e88db8a3
GIDEON PETERS <gideonpeters85@gmail.com>
2019-12-01 20:19:54 +0100

    challenge 1 complete

--- serverlessDreidel/index.js
+++ serverlessDreidel/index.js
@@ -1,16 +1,19 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+let driedel_array = ["ג - Gimmel", "ה - Hay", "ש - Shin", "נ - Nun"];
+let random_driedel =
+	driedel_array[Math.floor(Math.random() * (driedel_array.length - 1) + 1)];
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+module.exports = async function(context, req) {
+	// context.log("JavaScript HTTP trigger function processed a request.");
+	try {
+		context.res = {
+			status: 200,
+			body: random_driedel
+		};
+	} catch (error) {
+		context.res = {
+			status: 400,
+			body: "Something went wrong, try again",
+			error
+		};
+	}
 };
