d5762dfa59335fca24f3de28e88a16300e46bcf9
GIDEON PETERS <gideonpeters85@gmail.com>
2019-12-01 21:24:59 +0100

    made some updates

--- serverlessDreidel/index.js
+++ serverlessDreidel/index.js
@@ -1,13 +1,11 @@
-let driedel_array = ["ג - Gimmel", "ה - Hay", "ש - Shin", "נ - Nun"];
-let random_driedel =
-	driedel_array[Math.floor(Math.random() * (driedel_array.length - 1) + 1)];
-
 module.exports = async function(context, req) {
 	// context.log("JavaScript HTTP trigger function processed a request.");
 	try {
+		let driedel_array = ["ג - Gimmel", "ה - Hay", "ש - Shin", "נ - Nun"];
+		let random_driedel = Math.floor(Math.random() * driedel_array.length);
 		context.res = {
 			status: 200,
-			body: random_driedel
+			body: driedel_array[random_driedel]
 		};
 	} catch (error) {
 		context.res = {
